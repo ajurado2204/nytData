3b169662fa22486cc1a15a6f67b21df46497fcec
Alejandra Jurado <ajurado2204@gmail.com>
2016-03-13 11:40:16 -0400

    added panels for each nyt data

--- public/app.js
+++ public/app.js
@@ -4,14 +4,20 @@
 $(document).ready(function(){
 
   $.getJSON("/displayInfo", function(response) {
-    $("tbody").empty();
+
     response.forEach(function(news) {
-      var newTr = "<tr>";
-      newTr += "<td>" + news.headline + "</td>";
-      newTr += "<td><a href=" + 'news.articleLink' + ">" + news.articleLink + "</a></td>";
-      newTr += "<td>" + news.summary + "</td>";
-      newTr += "</tr>";
-      $("tbody").append(newTr);
+      var newDiv = "<div class='col-md-12'>";
+      newDiv += "<div class='panel panel-default'>";
+      newDiv += "<div class='panel-heading'>";
+      newDiv += "<h3 class='panel-title'>"+news.headline+"</h3>";
+      newDiv += "</div>";
+      newDiv += "<div class='panel-body'>";
+      newDiv += "<a href=" + 'news.articleLink' + ">" + news.articleLink + "</a>";
+      newDiv += "<p>"+news.summary+"</p></div>";
+      newDiv += "</div>";
+      newDiv += "</div>";
+
+      $(".container").append(newDiv);
 
     });
   });
--- server.js
+++ server.js
@@ -2,13 +2,14 @@
  * Created by Ale on 3/12/16.
  */
 var express = require('express');
-var app = express();
 var request = require('request');
 var cheerio = require('cheerio');
-
 var bodyParser = require('body-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose');
+var PORT = process.env.PORT || 8000;
+var app = express();
+
 
 app.use(logger('dev'));
 app.use(bodyParser.urlencoded({
@@ -16,7 +17,6 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(express.static('public'));
 
-var PORT = process.env.PORT || 8000;
 
 //Database configuration
 mongoose.connect('mongodb://localhost/week18day3mongoose');
@@ -28,6 +28,7 @@ db.on('error', function(err) {
 db.once('open', function() {
   console.log('Mongoose connection successful.');
 });
+
 
 //Require Schemas
 var NYTData = require('./models/theNytData.js');
@@ -53,16 +54,14 @@ app.get('/', function(req, res){
       insertedNYTData.save(function(err, doc) {
         if (err) {
           console.log(err);
-        } else {
-          console.log("Success");
         }
       });
-
     });
 
     res.sendfile(process.cwd() + '/index.html')
   });
 });
+
 
 app.get('/displayInfo', function(req, res) {
 
@@ -74,6 +73,7 @@ app.get('/displayInfo', function(req, res) {
     res.json(thedata);
   })
 });
+
 
 app.listen(PORT, function() {
   console.log('App running on port %s', PORT);
